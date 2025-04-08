d9c15f88e462fe532f0f8fcae6a5a3d97cfbc5aa
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2025-04-08 16:57:02 +0000

    add CHAPTER-04.md and pointer.c

---
+++ CHAPTER-04/pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char s1[] = "This is array.";
+char *s2 = "This is pointer.";
+
+int main()
+{
+    printf(" s1 = %p\n",  s1);
+    printf("&s1 = %p\n", &s1);
+    printf(" s2 = %p\n",  s2);
+    printf("&s2 = %p\n", &s2);
+    printf("sizeof(s1) = %ld\n", sizeof(s1));
+    printf("sizeof(s2) = %ld\n", sizeof(s2));
+    return 0;
+}
